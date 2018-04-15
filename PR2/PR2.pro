5d00e12f35f5114cfd32a7497c5739f42a9ba5de
Goga <goga.romanyuta@ukr.net>
2018-04-15 23:35:09 +0300

    Signed-off-by: Goga <goga.romanyuta@ukr.net>

---
+++ PR2/PR2.pro
@@ -0,0 +1,22 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2018-04-10T23:47:02
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = PR2
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp \
+    simc.cpp
+
+HEADERS += \
+    simc.h
---
+++ PR2/main.cpp
@@ -0,0 +1,87 @@
+﻿#include <QCoreApplication>
+#include <simc.h>
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    pfacility f,f2;
+    pqueue q,q2;
+       initlist(200);
+       newqueue(q,"Очередь1");
+       newqueue(q2,"Очередь2");
+       newfac(f,"Колонка1");
+       newfac(f2,"Колонка2");
+       while (systime <= 560) {
+           plan();
+           switch (sysevent) {
+           case 1:
+               create(20);
+               break;
+           case 2:
+               inqueue(q);
+               break;
+           case 3:
+               seize(f);
+               break;
+           case 4:
+               outqueue(q);
+               break;
+           case 5:
+               delayt(20);
+               break;
+           case 6:
+               if (t) wait(14);
+               break;
+           case 7:
+               outfac(f);
+               break;
+           case 8:
+               destroy();
+               break;
+           case 9:
+               create(15);
+               break;
+           case 10:
+               inqueue(q2);
+               break;
+           case 11:
+               seize(f2);
+               break;
+           case 12:
+               outqueue(q2);
+               break;
+           case 13:
+               delayt(20);
+               break;
+           case 14:
+               outfac(f2);
+               break;
+           case 15:
+               destroy();
+               break;
+           case 16:
+               create(16);
+               break;
+           case 17:
+               inqueue(q2);
+               break;
+           case 18:
+               seize(f2);
+               break;
+           case 19:
+               outqueue(q2);
+               break;
+           case 20:
+               delayt(20);
+               break;
+           case 21:
+               outfac(f2);
+               break;
+           case 22:
+               destroy();
+               break;
+           }
+
+       }
+       printall();
+    return a.exec();
+}
